d52fdc44ae71d73d8aa47ad4df274c39527385d6
horisziszi <horisziszi09@gmail.com>
2025-10-20 10:31:00 +0200

    10.20 (I had a terrible day, it doesn't work)

--- receptkonyv/Program.cs
+++ receptkonyv/Program.cs
@@ -1,55 +1,117 @@
 ﻿using System.Globalization;
 
-namespace Recept
+namespace receptkonyv
 {
     internal class Program
     {
-        // A hozzávalót leíró struktúra
+        struct Recept
+        {
+            public int sorszam;
+            public int hozzavalodb;
+            public int ertek;
+        }
+        //1.
         struct Hozzavalo
         {
-            public string Nev;        // hozzávaló neve
-            public int Mennyiseg;     // mennyiség (egység itt egész számként)
-            public double AranyAr;    // ára (aranyban, lebegőpontos)
-            public int ReceptSzam;    // melyik recepthez tartozik (azonosító)
+            public string nev;
+            public int db;
+            public double ar;
+            public int rec;
         }
-
-        // Beolvas: fájl sorait feldolgozza és visszaad egy listát Hozzavalo objektumokkal
-        static List<Hozzavalo> Beolvas(string path)
+        //2.
+        static List<Hozzavalo> Beolvas(string f)
         {
-            StreamReader reader = new StreamReader(path); // fájl megnyitása olvasásra
-            List<Hozzavalo> list = new List<Hozzavalo>();
-            Hozzavalo a;
-            string sor;
+            StreamReader fajl = new StreamReader(f);
+            List<Hozzavalo> a = new List<Hozzavalo>();
             string[] reszek;
-            while (!reader.EndOfStream) // amíg van sor a fájlban
+            int i;
+            Hozzavalo b;
+            while (!fajl.EndOfStream)
             {
-                sor = reader.ReadLine();                 // sor beolvasása
-                reszek = sor.Split(";");                 // mezők ; mentén szétválasztva
-                a.Nev = reszek[0];                       // 0. mező: név
-                a.Mennyiseg = Convert.ToInt32(reszek[1]); // 1. mező: mennyiség (int)
-                a.AranyAr = double.Parse(reszek[2], NumberFormatInfo.InvariantInfo); // 2. mező: ár (double)
-                a.ReceptSzam = Convert.ToInt32(reszek[3]); // 3. mező: recept száma (int)
-                list.Add(a);                              // hozzáadás a listához
+                b = new Hozzavalo();
+                reszek = fajl.ReadLine().Split(";");
+                b.nev = reszek[0];
+                b.db = Convert.ToInt32(reszek[1]);
+                b.ar = Double.Parse(reszek[2], NumberFormatInfo.InvariantInfo);
+                //Console.WriteLine(b.ar);
+                b.rec = Convert.ToInt32(reszek[3]);
+                a.Add(b);
             }
-            reader.Close(); // fájl lezárása (jobb lenne using blokkot használni)
-            return list;    // visszaadjuk a feltöltött listát
-        }
 
+            return a;
+        }
+        //kiirás
+        static void Kiiras(List<Hozzavalo> a)
+        {
+            int i;
+            for (i=0;i < a.Count;i++)
+            {
+                Console.WriteLine("{0,17} {1} {2,4} {3}", a[i].nev, a[i].db, a[i].ar, a[i].rec);
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
-            List<Hozzavalo> list = new List<Hozzavalo>();
-            list = Beolvas("recept.txt"); // beolvassuk a recept.txt fájlt a listába
-            int a;      // deklarált, de nincs használva a jelenlegi kódban
-            double m;   // próbálja a minimum ár indexét vagy értékét tárolni (hibás használat)
-            string s;   // név tárolására, de nincs inicializálva a kódban -> hibás
+            Console.Clear();
+            Hozzavalo olcso, olcsegy;
+            List<Hozzavalo> recept = Beolvas("recept.txt");
+            List<Hozzavalo> rec1 = new List<Hozzavalo>();
+            List<Hozzavalo> tizfol = new List<Hozzavalo>();
+            List<Hozzavalo> csokkeno = new List<Hozzavalo>();
+            Kiiras(recept);
+            int i,recdb = 0;
 
-            //2.
-            // Következő sorok hibásak:
-            // - list.IndexOf(list.Min(x => x.AranyAr)): IndexOf egy objektumot vár, de list.Min visszaad egy double értéket.
-            // - m változó típusa double, de IndexOf int-et adna vissza (ha működne).
-            // - s nincs beállítva mielőtt kiírnánk a nevét.
-            m = list.IndexOf(list.Min(x => x.AranyAr));
-            Console.WriteLine("A {0} hozzávaló a legolcsóbb {1} aranyba kerül.", s, list.Min(x=> x.AranyAr));
+            //3.
+            olcso = recept.Find(x=> x.ar == recept.Min(x=> x.ar));
+            Console.WriteLine("Legolcsóbb hozzávaló: {0}; ára: {1}",olcso.nev, olcso.ar);
+            //4.
+            olcsegy = recept.Find(x => x.ar/x.db == recept.Max(x => x.ar/x.db));
+            Console.WriteLine("Legdrágább hozzávaló (egységárban): " + olcsegy.nev);
+            //5.
+            Console.WriteLine("Hozzávalók összértéke: {0} arany", recept.Sum(x=> x.ar));
+            //6.
+            rec1 = recept.FindAll(x => x.rec == 1);
+            Console.WriteLine("\nRecept 1 hozzátevői:");
+            Kiiras(rec1);
+            Console.WriteLine("Összérték: {0} arany", rec1.Sum(x=> x.ar));
+            //7.
+            Console.WriteLine("Átlagos hozávaló ár: {0} arany", recept.Average(x => x.ar));
+            //8.
+            tizfol = recept.FindAll(x => x.ar > 10);
+            Console.WriteLine("10 arany fölötti hozzávalók:");
+            Kiiras(tizfol);
+            //9.
+            Console.WriteLine("Hozzávalók árban csökkenő sorrendben:");
+            csokkeno = recept.OrderBy(x=>x.ar).ToList();
+            csokkeno.Reverse();
+            Kiiras(csokkeno);
+            //10.
+            recdb = recept.DistinctBy(x => x.rec).Count();
+            Console.WriteLine("Különböző receptek száma: " + recdb);
+
+            //11.
+            int legdragabb=0;
+            for (i = 0; i < recept.Max(x => x.rec); i++)
+            {
+
+            }
+
+
+            Recept a;
+            List<Recept> receptek = new List<Recept>();
+            for (i = 0; i < recept.Max(x => x.rec); i++)
+            {
+                a = new Recept();
+                a.sorszam = 0;
+                a.hozzavalodb = 0;
+                //a.ertek = Hozzavalo..Where(x => x.rec == rec.ar).Sum(x => x.ar);
+                receptek.Add(a);
+            }
+            
+
+
+
+            Console.ReadKey();
         }
     }
 }
