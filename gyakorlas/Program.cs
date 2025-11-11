c29486aad4d5abf464f2b924e457bcd1dcb237fc
horisziszi <horisziszi09@gmail.com>
2025-11-11 22:45:11 +0100

    Update Program.cs

--- gyakorlas/Program.cs
+++ gyakorlas/Program.cs
@@ -9,7 +9,7 @@ class Program
 
         // https://www.oktatas.hu/pub_bin/dload/kozoktatas/tanulmanyi_versenyek/oktv/oktv2020_2021_1ford/info2_flap1f_oktv_2021.pdf
 
-        int zold = 1, piros = 1, sarga = 2, feher = 3;
+        /*int zold = 1, piros = 1, sarga = 2, feher = 3;
         int[] T = new int[6];
         T[0] = 1; // one way to build height 0 (no boxes)
         int[] heights = { zold, piros, sarga, feher };
@@ -24,8 +24,12 @@ class Program
         }
 
         for (int i = 0; i < T.Length; i++)
-            Console.WriteLine(T[i]);
+            Console.WriteLine(T[i]);*/
         // I've had enough after one excercise, I'm done :(
+
+
+
+        
 
         Console.ReadKey();
     }
