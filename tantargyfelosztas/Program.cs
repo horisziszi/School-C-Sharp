a73cc6caf5cac758f7bb330924b52d1c3e88eae6
horisziszi <horisziszi09@gmail.com>
2026-04-30 09:04:56 +0200

    04.30

--- tantargyfelosztas/Program.cs
+++ tantargyfelosztas/Program.cs
@@ -25,9 +25,25 @@ class Program
 
         return oraosszeg;
     }
+
+    static int negyedikfeladat(List<Adatok> felosztas, string tanarnev)
+    {
+        int oraosszeg = 0;
+        for (int i = 0; i < felosztas.Count; i++)
+        {
+            if (felosztas[i].tanar == tanarnev)
+            {
+                oraosszeg += felosztas[i].oraszam;
+            }
+        }
+
+        return oraosszeg;
+    }
     static void Main(string[] args)
     {
         Console.Clear();
+
+        // 1. feladat
         StreamReader fajl = new StreamReader("beosztas.txt");
         Adatok adatok = new Adatok();
         List<Adatok> felosztas = new List<Adatok>();
@@ -42,10 +58,34 @@ class Program
 
         }while (!fajl.EndOfStream);
 
+
+        // 2. feladat
         Console.WriteLine("Bejegyzések száma: " + masodikfeladat(felosztas));
+        Console.WriteLine();
 
+
+        // 3. feladat
         Console.WriteLine("Heti órák száma: " + hardmadikfeladat(felosztas));
+        Console.WriteLine();
 
+        // 4. feladat
+        Console.Write("Kérem a tanár nevét: ");
+        Console.WriteLine("Heti órák száma: " + negyedikfeladat(felosztas, Console.ReadLine()));
+
+        // 5. feladat
+        StreamWriter ki = new StreamWriter("of.txt");
+        for (int i = 0; i< felosztas.Count; i++)
+        {
+            if (felosztas[i].nev == "osztalyfonoki")
+            {
+                ki.WriteLine(felosztas[i].osztaly + " - " + felosztas[i].tanar);
+            }
+        }
+        ki.Close();
+        Console.WriteLine("Az of.txt fájl elkészült!");
+
+
+        
 
         Console.ReadKey();
     }
